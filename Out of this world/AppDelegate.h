ec30a4c66ff0076acf02d46d1868731cddfa07ed
delmarz <delmarz@delmarzs-Mac-Pro.local>
2015-01-22 13:41:12 +0800

    Initial Commit

---
+++ Out of this world/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Out of this world
+//
+//  Created by delmarz on 1/22/15.
+//  Copyright (c) 2015 delmarz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Out of this world/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Out of this world
+//
+//  Created by delmarz on 1/22/15.
+//  Copyright (c) 2015 delmarz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
